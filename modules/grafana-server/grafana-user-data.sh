005cbdfbf7227e30ccfd0a75b3681de892d7b8c1
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-07-20 11:08:05 -0500

    PLAT-262: add the terraform templates

---
+++ modules/grafana-server/grafana-user-data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+echo "${some_var}"
---
+++ modules/grafana-server/outputs.tf
@@ -0,0 +1,12 @@
+output "security_group_id" {
+  value = "${aws_security_group.grafana_security_group.id}"
+}
+
+
+output "private_ip" {
+  value = "${aws_instance.grafana_instance.private_ip}"
+}
+
+output "http_port" {
+  value = "${var.grafana_http_port}"
+}
